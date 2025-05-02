92f9a3ec8d5574cc95fb25c4092ba756161c0af0
LDLcodes <153458380+LDLcodes@users.noreply.github.com>
2025-05-03 00:24:42 +0900

    setting up react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap"; // Assuming Container is from react-bootstrap
 
 function App() {
   return (
     <>
-      <h1>Hi</h1>
+      <Container className="my-4">
+        {" "}
+        {/* margin on the top and buttom */}
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>New</h1>} />
+          <Route path="/:id">
+            <Route index element={<h1>How</h1>} />
+            <Route path="edit" element={<h1>Edit</h1>} />
+            <Route />
+          </Route>
+          {/* if you type wrong path you will directed to home "/" */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Container>
     </>
   );
 }
